45314611f3dfc272ff8771d3f3215944a3e98565
chrisdionisius <chrisdionisius@gmail.com>
2023-04-03 15:07:10 +0700

    using list view (FlatList)

--- App.tsx
+++ App.tsx
@@ -1,27 +1,76 @@
 import React from "react";
-import { Image, ScrollView, Text} from 'react-native';
+import { FlatList, StyleSheet, Text, View} from 'react-native';
 
-const logo = {
-  uri: 'https://reactnative.dev/img/tiny_logo.png',
-  width : 64,
-  height : 64,
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    paddingTop:22,
+  },
+  item:{
+    padding:10,
+    fontSize:18,
+    height:44,
+  }
+})
+
+const FlatListBasic = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList 
+        data={[
+          {key: 'Devin'},
+          {key: 'Dan'},
+          {key: 'Dominic'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+          {key: 'Yohan'},
+          {key: 'Matt'},
+          {key: 'Riz'},
+          {key: 'Timothy'},
+          {key: 'Fany'},
+        ]}
+        renderItem={({item}) => 
+          <Text style={styles.item}>
+            {item.key}
+          </Text>
+        }
+        />
+    </View>
+  );
 }
 
-const App = () =>(
-  <ScrollView>
-    <Text style={{fontSize:96}}>Scroll plzz</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{fontSize:96}}>Again, perhaps ?</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{fontSize:96}}>Scrolling down</Text>
-  </ScrollView>
-);
+export default FlatListBasic;
 
-export default App;
+
+// import React from "react";
+// import { Image, ScrollView, Text} from 'react-native';
+
+// const logo = {
+//   uri: 'https://reactnative.dev/img/tiny_logo.png',
+//   width : 64,
+//   height : 64,
+// }
+
+// const App = () =>(
+//   <ScrollView>
+//     <Text style={{fontSize:96}}>Scroll plzz</Text>
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Text style={{fontSize:96}}>Again, perhaps ?</Text>
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Text style={{fontSize:96}}>Scrolling down</Text>
+//   </ScrollView>
+// );
+
+// export default App;
 
 // import React , {Component} from 'react';
 // import {Button, Image, Text, View} from 'react-native';
